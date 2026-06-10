6ee594628cb0aab1f3f2cfc372c075edeaec0698
QWE-Ult <yashmeghnathi07@gmail.com>
2026-06-11 02:35:54 +0530

    Add factors program

---
+++ Practice/Factors.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        print(i)
+        
+        
+# n=int(input())
+
+# i=1
+# while i <=n+1:
+
+#     if n%i==0
+#     print(i)
+# i+=1
