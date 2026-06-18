badb98bc626e3f517958958038d35a31dbc11353
QWE-Ult <yashmeghnathi07@gmail.com>
2026-06-18 18:24:51 +0530

    Add ATM Menu Program project

---
+++ Basic Beginner_Projects/Basic_ATM_Menu_Program.py
@@ -0,0 +1,34 @@
+Balance = 1000000
+
+while True:
+    print("### ATM Menu ###\n")
+    print("\t1. Check Balance")
+    print("\t2. Deposit Money")
+    print("\t3. Withdraw Money")
+    print("\t4. Exit")
+
+    a = int(input("\nChoose From Above : "))
+
+    if a == 1:
+        print(f"Your Balance is : {Balance}")
+
+    elif a == 2:
+        i = int(input("Enter Amount : "))
+        Balance = Balance + i
+        print(f"You Deposited : {i} and Your Current Balance is {Balance}")
+
+    elif a == 3:
+        i = int(input("Enter Amount : "))
+
+        if i <= Balance:
+            Balance = Balance - i
+            print(f"You withdrew : {i} and Your Current Balance is {Balance}")
+        else:
+            print("Insufficient Balance")
+
+    elif a == 4:
+        print("Thank you for using the ATM.")
+        break
+
+    else:
+        print("Insert a Valid Number")
